35217d0bcc6d959215701cb13e048a0a94af4a34
Ibrahim Muhammad <ibrahim_beem@icloud.com>
2017-09-21 21:54:08 +0200

    Add "consulting-firm"

---
+++ consulting-firm/assets/js/main.js
@@ -0,0 +1,55 @@
+//cover height
+function resizeContent() {
+	// var diamondWidth = $('#main-nav a').width()
+	// $('#main-nav a').height(diamondWidth)
+}
+
+
+$(document).ready(function() {
+
+	resizeContent()
+	
+	//attach on resize event
+	$(window).resize(function() {
+		resizeContent()
+	});
+
+	$('.slide').each(function() {
+		var img = $('> img', this)
+		$(this).backstretch(img.attr('src'))
+		img.hide()
+	})
+
+	$('.slider').slick({
+		autoplay: true,
+		infinite: false,
+		arrows: false,
+		speed: 600,
+		cssEase: 'cubic-bezier(0.86, 0, 0.07, 1)',
+		slidesToShow: 1,
+		slidesToScroll: 1,
+		onInit: function(slider) {
+		$('.slider a.prev').hide()
+		},
+		onAfterChange: function(slider,index) {
+			if(index === 0) {
+				$('.slider a.prev').fadeOut(100)
+				$('.slider a.next').fadeIn(100)
+			} else if(index === slider.slideCount-1) {
+				$('.slider a.next').fadeOut(100)
+				$('.slider a.prev').fadeIn(100)
+			} else {
+				$('.slider a.prev, .slider a.next').fadeIn(100)
+			}
+		},
+	})
+
+	//manual buttons
+	$('.slider .next').on('click', function() {
+	$(this).parent().slickNext()
+	})
+	$('.slider .prev').on('click', function() {
+	$(this).parent().slickPrev()
+	})
+
+})
